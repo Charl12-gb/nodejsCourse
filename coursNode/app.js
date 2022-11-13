682be035cd5378b5cc1a1db9ddacd52ad7e246e0
Charles GBOYOU <gboyoucharles22@gmail.com>
2022-11-13 15:07:16 +0100

    Add | update | delete

--- coursNode/app.js
+++ coursNode/app.js
@@ -1,19 +1,83 @@
 const express = require('express');
-const { status } = require('express/lib/response');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+
+const optionBd = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'notes_bd'
+};
+
 const app = express();
+
+//Extration des données du formulaire
+app.use(express.urlencoded({ extended: false }));
+
+//Définition de la connexion
+app.use(myConnection(mysql, optionBd, 'pool'));
 
 // Définition du moteur d'affichage ejs
 app.set("view engine", "ejs");
 app.set("views", "IHM");
 
 app.get('/', (req, res) => {
-    const dateVisite = Date().toString();
-    const notes = [
-        { title: "Title 1", desc: "1- Lorem ipsum dolor sit amet consectetur adipisicing elit." },
-        { title: "Title 2", desc: "2- Lorem ipsum dolor sit amet consectetur adipisicing elit." },
-        { title: "Title 3", desc: "3- Lorem ipsum dolor sit amet consectetur adipisicing elit." }
-    ];
-    res.status(200).render("index", { dateVisite, notes });
+
+    req.getConnection((erreur, connection) => {
+        if (erreur) {
+            console.log(erreur);
+        } else {
+            connection.query('SELECT * FROM notes', [], (erreur, resultat) => {
+                if (erreur) {
+                    console.log(erreur);
+                } else {
+                    res.status(200).render('index', { resultat });
+                }
+            });
+        }
+    });
+});
+
+app.post('/notes', (req, res) => {
+    let id = req.body.id === "" ? null : req.body.id;
+    let title = req.body.title;
+    let description = req.body.description;
+
+    let reqSql = id === null ? 'INSERT INTO notes(id, title, description) VALUES(?, ?, ?)' : 'UPDATE notes SET title = ?, description = ? WHERE id = ?';
+    let donnees = id === null ? [null, title, description] : [title, description, id];
+
+    req.getConnection((erreur, connection) => {
+        if (erreur) {
+            console.log(erreur);
+        } else {
+            connection.query(reqSql, donnees, (erreur, resultat) => {
+                if (erreur) {
+                    console.log(erreur);
+                } else {
+                    res.status(300).redirect("/");
+                }
+            });
+        }
+    });
+});
+
+app.delete('/notes/:id', (req, res) => {
+    let id = req.params.id;
+
+    req.getConnection((erreur, connection) => {
+        if (erreur) {
+            console.log(erreur);
+        } else {
+            connection.query("DELETE FROM notes where id = ?", [id], (erreur, resultat) => {
+                if (erreur) {
+                    console.log(erreur);
+                } else {
+                    res.status(200).json({ routeRacine: "/" });
+                }
+            });
+        }
+    });
 });
 
 app.get('/about', (req, res) => {
